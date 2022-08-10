0bbf8858180d7eee34d0bc858a91fe5a502700d8
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-08-10 21:50:49 +1200

    Allow external references to be more than text.

--- src/components/templates/Reference.vue
+++ src/components/templates/Reference.vue
@@ -3,7 +3,14 @@
     {{ node.textContent }}
   </router-link>
   <a v-else :href="node.getAttribute('refuri')" target="_blank">
-    {{ node.textContent }}
+    <component
+      v-for="(c, index) in children"
+      :key="'sub_link_' + index"
+      :is="c.component"
+      :node="c.node"
+      :componentName="c.name"
+      :properties="c.properties"
+    />
   </a>
 </template>
 
@@ -11,6 +18,7 @@
 import { onMounted, toRefs, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
+import { useChildren } from '../../composables/computed'
 import { useMethods } from '../../composables/methods'
 import { useBaseReference } from '../../composables/basereference'
 
@@ -24,13 +32,17 @@ const props = defineProps({
   },
 })
 
-const routeDescription = ref({path: '', hash: ''})
+const routeDescription = ref({ path: '', hash: '' })
 const { node } = toRefs(props)
 const route = useRoute()
 
 const { isInternalReference } = useMethods()
-const { onReferenceCreated } = useBaseReference(node.value, route, routeDescription)
+const { onReferenceCreated } = useBaseReference(
+  node.value,
+  route,
+  routeDescription,
+)
 
 onMounted(onReferenceCreated)
-
+const { children } = useChildren(node)
 </script>
